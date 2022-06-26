7f6c60e2efcb8fd2aa25a17b1d82b35bf9fa17e8
Yakup KAYA <iamyakupkaya@outlook.com>
2022-06-26 13:21:09 +0300

    init project and using ejs with project to achieve data from html page with ejs

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path"); // klasör yolunu tam olarak her işletim sisteminde alabilmek için.
+const ejs = require("ejs");
+const app = express();
+console.log(path.resolve(__dirname));
+
+// Ejs, html şablonlarıyla, servis tarafımızdan gelen verileri birleştirebilmemizi sağlayan javascript kütüphanesidir.
+app.set("view engine", "ejs");
+
+const kisilerDizisi = [
+  { ad: "yakup1", id: 1 },
+  { ad: "yakup2", id: 2 },
+  { ad: "yakup3", id: 3 },
+  { ad: "yakup4", id: 4 },
+];
+const dersAdi = "NodeJS with EJS";
+const yas = 27;
+const renkler = ["krımızı", "mavi", "gri", "siyah", "mor"];
+const site = "<h1>Bu Tire ile yapılan EJS</h1>";
+//general get
+app.get("/", (reqeust, response) => {
+  //path.resolve(__dirname) aslında bize şuanda bu klaöürün tam yolunu veririr. C:\Users\Yakup KAYA\Desktop\NodeJS-ProjectCategory
+  //...sonuna eklenen "index.html ise o path yolunu bulduktan sonra sonuna verilen ikinci argümanı ekleme demektir."
+  //response.sendFile(path.resolve(__dirname, "index.html"));
+  response.render("index.ejs", {
+    kisiler: kisilerDizisi,
+    ders: dersAdi,
+    yas,
+    renkler,
+    site,
+  }); // artık buraya kendi verilerimi ekleyebilirim.
+});
+
+app.listen(3000, (_) => console.log("Server is initiliazed on 3000 port"));
